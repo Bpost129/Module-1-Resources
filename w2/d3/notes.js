33972e897a79bbd25b56381c6d2f7b1facc1c7a2
Billy Post <Bpost129@yahoo.com>
2022-03-23 10:50:32 -0600

    notes

--- w2/d3/notes.js
+++ w2/d3/notes.js
@@ -4,8 +4,35 @@ let pokemons = pokemonCreator();
 
 // console.log(pokemonCreator());
 
-let displayPokemon = function(pokemons) {
-    return pokemons;
+let displayPokemon = function(pokemons, cb) {
+    if (!cb) return pokemons;
+    let results = cb(pokemons);
+    return results;
 }
 
-console.log(displayPokemon(pokemons));
+let names = function(pokemons) {
+    return pokemons.map(function(pokemon) {
+        return pokemon.name;
+    })
+}
+
+let fires = function(pokemons) {
+    return pokemons.filter(function(pokemon) {
+        return pokemon.type === "Fire";
+    })
+}
+
+let levels = function(pokemons) {
+    return pokemons.reduce(function(count, pokemon) {
+        if (pokemon.level >= 30 && pokemon.level <= 50) {
+            return count + 1;
+        } else {
+            return count;
+        }
+    }, 0)
+}
+
+// console.log(displayPokemon(pokemons));
+// console.log(displayPokemon(pokemons, names));
+// console.log(displayPokemon(pokemons, fires));
+console.log(displayPokemon(pokemons, levels));
